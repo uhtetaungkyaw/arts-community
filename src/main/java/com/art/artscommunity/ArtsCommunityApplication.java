f115f8b1a052cb11c22afdd29438af0bd0327b72
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-03-18 11:06:45 +0630

    Initial commit

---
+++ src/main/java/com/art/artscommunity/ArtsCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.art.artscommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtsCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtsCommunityApplication.class, args);
+    }
+
+}
